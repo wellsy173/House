3714b9634ea2063f176e5786f9f3651d1c35dee8
wellsy173 <62893834+wellsy173@users.noreply.github.com>
2020-07-07 16:54:07 +0800

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+let json = """
+[
+{
+
+"type": "colonial",
+"location": "Plainville, MA",
+"bedrooms": 3,
+"bathrooms": 2.5,
+"has air conditioning": false,
+"amenities": ["basement", "garden"],
+"listing": {
+    "price": 430000,
+    "date": "May 2018"
+}
+},
+
+{
+"type": "condo",
+"location": "San Francisco, CA",
+"bedrooms": 1,
+"bathrooms": 1,
+"has air conditioning": true,
+"amenities": []
+}
+]
+
+""".data(using: .utf8)!
+
+struct Listing: Codable {
+    let price: Int
+    let date: String
+}
+
+struct House: Codable {
+    let houseType: String
+    let location: String
+    let beds: Int
+    let baths: Float
+    let hasAirConditioning: Bool
+    let amenities: [String]
+    let listing: Listing
+    
+    enum CodingKeys: String, CodingKey {
+        case houseType = "type"
+        case location = "location"
+        case beds = "bedrooms"
+        case baths = "bathrooms"
+        case hasAirConditioning = "has air conditioning"
+        case amenities
+        case listing
+        
+    }
+}
+
+let decoder = JSONDecoder()
+do {
+    let house = try decoder.decode([House] .self, from: json)
+    print(house)
+}catch{
+    print(error)
+}
+
